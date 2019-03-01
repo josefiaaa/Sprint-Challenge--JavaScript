981222a6593e04460dc06a33d87aefa93bbda84d
josefiaaa <josefiamagagnoli@gmail.com>
2019-03-01 13:51:32 -0500

    Arrays and Functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,107 +1,120 @@
-// // ==== Objects ====
+// ==== Objects ====
 
-// /* 
-//   Given the following information about dinosaurs, create 3 objects: 
-//   Use this pattern to create your objects: 
-//   object name, diet, weight, length, period
-// */
+/* 
+  Given the following information about dinosaurs, create 3 objects: 
+  Use this pattern to create your objects: 
+  object name, diet, weight, length, period
+*/
 
-// // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
-
-// const ranno = {
-//   name: 'tyrannosaurus',
-//   diet: 'carnivorous',
-//   weight: '7000kg',
-//   length: '12m',
-//   period: 'Late Cretaceious',
-//   roar: function () {
-//     return "RAWERSRARARWERSARARARRRR!"  // ?????
-//   }
-
-// }
-
-// // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
-// const steg = {
-//   name: 'stegosaurus',
-//   diet: 'herbivorous',
-//   weight: '2000kg',
-//   length: '9m',
-//   period: 'Late Jurassic',
-// }
-
-// // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
-// const velo = {
-//   name: 'velociraptor',
-//   diet: 'carnivorous',
-//   weight: '15kg',
-//   length: '1.8m',
-//   period: 'Late Cretaceious',
-// }
-
-// // Using your dinosaur objects, log answers to these questions:
-
-// // How much did tyrannosaurus weigh?
-// console.log(ranno.weight);
-
-// // What was the diet of a velociraptor?
-// console.log(velo.diet);
-
-// // How long was a stegosaurus?
-// console.log(steg.length);
-
-// // What time period did tyrannosaurus live in?
-// console.log(ranno.period);
+// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
 
-// // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-// console.log(ranno.roar);
+const ranno = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious',
+  roar: function () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+
+}
+
+// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+
+const steg = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
+
+// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+const velo = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious',
+}
+
+// Using your dinosaur objects, log answers to these questions:
+
+// How much did tyrannosaurus weigh?
+console.log(ranno.weight);
+
+// What was the diet of a velociraptor?
+console.log(velo.diet);
+
+// How long was a stegosaurus?
+console.log(steg.length);
+
+// What time period did tyrannosaurus live in?
+console.log(ranno.period);
 
 
-// // ==== Arrays ====
-
-// // Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
-
-// const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-// {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-// {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-// {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-// {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-// {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-// {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-// {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-// {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-// {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
-
-// /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-// Once you have the new array created, sort the universities alphabetically and log the result. */
-// const universities = [];
-// console.log(universities);
-
-// /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
-
-// The resulting contact information should have a space between the first name and the email information like this: 
-// Name email@example.com
-
-// Log the result of your new array. */
-
-// const contactInfo = [];
-
-// graduates.forEach(function(grad) {
-//   let contact = (`${grad.first_name} ${grad.email}`);
-//   contactInfo.push(contact);
-// });
-
-// console.log(contactInfo);
+// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+console.log(ranno.roar());
 
 
-// /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+// ==== Arrays ====
 
-// const uni = [];
+// Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
 
-// console.log(uni);
+const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+Once you have the new array created, sort the universities alphabetically and log the result. */
+const universities = [];
+
+graduates.forEach(function(graduate){
+    universities.push(graduate.university);
+});
+
+universities.sort();
+
+console.log(universities);
+
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+
+The resulting contact information should have a space between the first name and the email information like this: 
+Name email@example.com
+
+Log the result of your new array. */
+
+const contactInfo = [];
+
+graduates.forEach(function(grad) {
+  let contact = (`${grad.first_name} ${grad.email}`);
+  contactInfo.push(contact);
+});
+
+console.log(contactInfo);
+
+
+/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
+const uni = [];
+
+graduates.forEach(function(graduate){
+    if (graduate.university.includes('Uni')) {
+        uni.push(graduate.university);
+    }
+});
+
+console.log(uni);
 
 
 // // ==== ADVANCED Array Methods ====
